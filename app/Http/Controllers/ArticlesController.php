d22f7bcbbdab896617e3122ee32bca733c1d1741
conradsuuna <conradsuuna.cs@gmail.com>
2023-06-06 16:44:48 +0300

    modularise components

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\UserSettings;
+use Illuminate\Support\Facades\Http;
+
+class ArticlesController extends Controller
+{
+    //
+    public function getNewsAPISources(Request $request) {
+        try{
+            $news_sources = [];
+            $news_categories = [];
+
+            $api_key = env('NEWS_API_KEY');
+            $url = "https://newsapi.org/v2/top-headlines/sources?apiKey={$api_key}";
+            $results = Http::get($url);
+            $sources = json_decode($results->getBody(), true);
+
+            foreach ($sources['sources'] as $source) {
+                $news_sources[] = array(
+                    'id' => $source['id'],
+                    'name' => $source['name'],
+                );
+
+                // only add unique categories
+                if (!in_array(array('category' => $source['category'],), $news_categories)) {
+                    $news_categories[] = array(
+                        'category' => $source['category'],
+                    );
+                }
+            }
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'News sources fetched successfully',
+                'news_sources' => $news_sources,
+                'news_categories' => $news_categories,
+            ], 200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+
+    public function getNewsAPINews(Request $request)
+    {
+        try{
+            $guard = Auth::guard('sanctum');
+            $api_key = env('NEWS_API_KEY');
+            // get query params
+            $query_params = $request->query();
+            $q = $request->has('q') ? $query_params['q'] : 'money+sports';
+            $sortBy = $request->has('publishedAt') ? $query_params['publishedAt'] : date("Y-m-d");
+            $sources = $request->has('sources') ? $query_params['sources'] : 'bbc-news';
+
+            if ($guard->check()) {
+                $user = $guard->user();
+                $user_settings = UserSettings::where('user_id', $user->id)->first();
+                $user_settings->prefered_source = json_decode($user_settings->prefered_source);
+                $user_settings->category = json_decode($user_settings->category);
+                $user_settings->authors = json_decode($user_settings->authors);
+
+                // personalisation focuses on sources, categories and authors
+                // params: country, category, sources, q, pageSize, page, apiKey, publishedAt
+                $stored_sources = $request->has('sources') ? $sources :
+                    implode(',', $user_settings->prefered_source);
+                $category = $user_settings->category;
+                $authors = $user_settings->authors;
+
+                $url = "https://newsapi.org/v2/everything?sources={$stored_sources}&sortBy={$sortBy}&apiKey={$api_key}";
+                if ($q) {
+                    $url .= "&q={$q}";
+                }
+
+                $results = Http::get($url);
+                $all_articles = json_decode($results->getBody(), true);  
+
+                // filter articles based on user preferences
+                if (count($authors) > 0) {
+                    $articles = $all_articles['articles'];
+                    $filtered_articles = [];
+                    foreach ($articles as $article) {
+                        if (in_array($article['author'], $authors)) {
+                            array_push($filtered_articles, $article);
+                        }
+                    }
+    
+                    $all_articles['articles'] = $filtered_articles;
+                    $all_articles['totalResults'] = count($filtered_articles);
+                }
+
+                $response = $all_articles;
+            } else {
+                // search results by date, category and source
+                // params: source, category, publishedAt, q
+                $url = "https://newsapi.org/v2/everything?sources={$sources}&sortBy={$sortBy}&apiKey={$api_key}";
+                if ($q) {
+                    $url .= "&q={$q}";
+                }
+
+                $results = Http::get($url);
+                $all_articles = json_decode($results->getBody(), true); 
+
+                $response = $all_articles;
+            }
+
+            return response()->json($response['articles'], 200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'status'=> 'error',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function getGuardianNews (Request $request) {
+        $guard = Auth::guard('sanctum');
+
+        if ($guard->check()) {
+            $user = $guard->user();
+            $user_settings = UserSettings::where('user_id', $user->id)->first();
+
+            $api_key = env('GUARDIAN_API_KEY');
+            // personalisation focuses on sources, categories and authors
+            // params: country, tag, sources, q, pageSize, page, apiKey, from-date and contributor
+            $query_url = "https://content.guardianapis.com/search?api-key=$api_key";
+            
+            $data = $response->json();
+        } else {
+            // search results by date, category and source
+            // params: source, category, publishedAt, q
+            $user = "good";
+        }
+
+        return response()->json([
+            'settings' => $user,
+        ], 200);
+    }
+
+    public function getNYTimesNews (Request $request) {
+        $guard = Auth::guard('sanctum');
+
+        if ($guard->check()) {
+            $user = $guard->user();
+            $user_settings = UserSettings::where('user_id', $user->id)->first();
+
+            $api_key = env('NYTIMES_API_KEY');
+            // personalisation focuses on sources, categories and authors
+            // params: glocations, tag, source, q, pageSize, page, apiKey, pub-date and persons
+            $query_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=$api_key";
+            
+            $data = $response->json();
+        } else {
+            // search results by date, category and source
+            // params: source, category, publishedAt, q
+            $user = "good";
+        }
+
+        return response()->json([
+            'settings' => $user,
+        ], 200);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserSettingsController; 
+use App\Http\Controllers\ArticlesController; 
 
 
 /*
@@ -23,11 +24,9 @@ Route::controller(UserController::class)->group(function(){
     Route::post('/logout', 'logout')->middleware('auth:sanctum');
 });
 
-Route::controller(UserSettingsController::class)->group(function(){
-    Route::get('settings', 'getUserSettings');
-    Route::post('add-preference', 'saveUserPreferences')->middleware('auth:sanctum');
+Route::controller(ArticlesController::class)->group(function(){
     Route::get('articles-from-newa-api', 'getNewsAPINews');
-
+    Route::get('sources-from-newa-api', 'getNewsAPISources');
 });
 
 // add protected routes here
@@ -35,4 +34,5 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    Route::post('add-preference', [UserSettingsController::class, 'saveUserPreferences']);
 });
